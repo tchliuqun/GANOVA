477cb4369080090a79f7907ee4d36e7c3b4ce8a7
tchliuqun <tchliuq@163.com>
2018-03-04 12:43:18 +0800

    test writer for linux

--- src/main/scala/PLS/simumasterActor.scala
+++ src/main/scala/PLS/simumasterActor.scala
@@ -40,9 +40,11 @@ class simumasterActor(pms:Pms) extends Actor{
       val wrt = system.actorOf(paraWriterActor.props(fileName(this.ofile)), wname)
       writer = Some(wrt)
 
+      println("starting writer")
+      getGlist(chr.chrname.apply(0))
       //wrt ! myParallel.paraWriterActor.WriteStr("dispatch starting 1")
       wrt ! myParallel.paraWriterActor.totalNumber(glists.length * times * H.length)
-      writer.foreach(_ ! myParallel.paraWriterActor.WriteStr("dispatch starting"))
+      //writer.foreach(_ ! myParallel.paraWriterActor.WriteStr("dispatch starting"))
       println("starting processing")
       if (glists.length < cores){
         cores = glists.length
