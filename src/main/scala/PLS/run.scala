32e0fc6672864fe7f8b7993bd95b2bbb1f1e707c
tchliuqun <tchliuq@163.com>
2018-03-04 14:26:41 +0800

    test writer for linux

--- src/main/scala/PLS/run.scala
+++ src/main/scala/PLS/run.scala
@@ -55,6 +55,11 @@ object run extends App {
 //  withParallelism(2) {
 //    (1 to 100).par.map(_ * 2)
 //  }
+val orderpms = simumasterActor.Pms(gPms.rp + "simuRs.txt", 100, Array(0.01f, 0.03f, 0.05f))
+  val srt = system.actorOf(simumasterActor.props(orderpms), "srt")
+  println("start")
+  //implicit val timeout = Timeout(999 hours)
+  srt ! SnpProcessActor.chr(Array("15"))
 
 //  if (false) {
     val mb = 1024 * 1024
@@ -113,13 +118,9 @@ object run extends App {
 //    pc.tasksupport = new ForkJoinTaskSupport(new java.util.concurrent.ForkJoinPool(cores - 1))
 //    pc.foreach(simugenNo)
 //  }
-  if (false) {
-    val orderpms = simumasterActor.Pms(gPms.rp + "simuRs.txt", 100, Array(0.01f, 0.03f, 0.05f))
-    val srt = system.actorOf(simumasterActor.props(orderpms), "srt")
-    println("start")
-    //implicit val timeout = Timeout(999 hours)
-    srt ! SnpProcessActor.chr(Array("15"))
-  }
+//  if (false) {
+
+//  }
 //if (false) {
   val filn = myParallel.paraWriterActor.fileName(gPms.rp +"tests.txt")
   val filn1 = myParallel.paraWriterActor.fileName(gPms.rp +"tests1.txt")
